027aa4cc954cfb307a473ed4f06c40b73b1bf116
JesuFertez <maria.fernandez.cortez@gmail.com>
2024-11-14 16:46:37 -0300

    Inicialización de proyecto

---
+++ src/test/java/com/coworkers/clinicpet/ClinicPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworkers.clinicpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
