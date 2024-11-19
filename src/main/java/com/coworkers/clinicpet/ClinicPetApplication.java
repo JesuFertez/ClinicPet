03db65107cd91d401cbd68c67ec6e7777442e29d
illancapan <igorllancapan@gmail.com>
2024-11-19 17:27:45 -0300

    Actualizacion conexion h2, genera entidades, errores en servicio y controllers

--- src/main/java/com/coworkers/clinicpet/ClinicPetApplication.java
+++ src/main/java/com/coworkers/clinicpet/ClinicPetApplication.java
@@ -1,14 +1,26 @@
 package com.coworkers.clinicpet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ClinicPetApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(ClinicPetApplication.class, args);
-	}
+        SpringApplication.run(ClinicPetApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner run(Environment environment) {
+        return args -> {
+            String port = environment.getProperty("server.port","");
+            System.out.println("La API está corriendo en el puerto: " +port);
+            System.out.println("Listo ");
+        };
+
+    }
 }
