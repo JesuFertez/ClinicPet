2464cbb77a7aa741d6b05977193e9823b3d34ed7
illancapan <igorllancapan@gmail.com>
2024-11-18 21:12:35 -0300

    H2 databse configuration, working.

--- src/main/java/com/coworkers/clinicpet/ClinicPetApplication.java
+++ src/main/java/com/coworkers/clinicpet/ClinicPetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicPetApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ClinicPetApplication.class, args);
 	}
 
