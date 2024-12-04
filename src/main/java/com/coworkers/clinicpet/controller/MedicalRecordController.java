a340a2a7f8de9c81c5d3c2c6da42adfe061d48ef
illancapan <igorllancapan@gmail.com>
2024-12-04 15:27:59 -0300

    Get, post, update, delete

--- src/main/java/com/coworkers/clinicpet/controller/MedicalRecordController.java
+++ src/main/java/com/coworkers/clinicpet/controller/MedicalRecordController.java
@@ -1,6 +1,5 @@
 package com.coworkers.clinicpet.controller;
 
-import com.coworkers.clinicpet.model.dto.MedicalHistoryDTO;
 import com.coworkers.clinicpet.model.dto.MedicalRecordDTO;
 import com.coworkers.clinicpet.service.MedicalRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +33,17 @@ public class MedicalRecordController {
     public ResponseEntity<MedicalRecordDTO> createRecord(@RequestBody MedicalRecordDTO medicalRecordDTO) {
         MedicalRecordDTO createdRecord = medicalRecordService.createMedicalRecord(medicalRecordDTO);
         return new ResponseEntity<>(createdRecord, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MedicalRecordDTO> updateRecord(@PathVariable Long recordId, @RequestBody MedicalRecordDTO medicalRecordDTO){
+        MedicalRecordDTO updateRecord = medicalRecordService.updateRecord( recordId, medicalRecordDTO);
+        return ResponseEntity.ok(updateRecord);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecord(@PathVariable Long recordId){
+        medicalRecordService.deleteRecord( recordId);
+        return ResponseEntity.noContent().build();
     }
 }
